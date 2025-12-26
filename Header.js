37e7c8de2a3f951fc5368b8205251fb9c02c6911
Nx_erfan <erfansadeghi230@gmail.com>
2025-12-26 18:47:49 +0330

    ###

--- Header.js
+++ Header.js
@@ -1,16 +1,13 @@
 document.querySelectorAll('.Items .content').forEach(item => {
         item.addEventListener('click', function() {
-            // حذف کلاس active از تمام گزینه‌ها
             document.querySelectorAll('.Items .content').forEach(el => {
                 el.classList.remove('active');
             });
-            
-            // اضافه کردن کلاس active به گزینه‌ای که روی آن کلیک شده
+
             this.classList.add('active');
         });
     });
-    
-    // اختیاری: فعال کردن گزینه اول به صورت پیش‌فرض هنگام لود صفحه
+
     window.onload = () => {
         const firstItem = document.querySelector('.Items .content');
         if(firstItem) firstItem.classList.add('active');
